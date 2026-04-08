464a091ae751d18570c5055c4d8e993d3606be11
3lcreations <mauro7leo10@gmail.com>
2026-04-08 21:00:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,3 +12,29 @@ self.addEventListener('fetch', (event) => {
     // Lasciamo che il sito comunichi normalmente con Internet e Firebase
     event.respondWith(fetch(event.request));
 });
+// Ascolta i messaggi push in arrivo
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : { title: '3L Creations', body: 'Nuova notifica!' };
+    
+    const options = {
+        body: data.body,
+        icon: 'favicon.png', // L'icona della tua app
+        badge: 'favicon.png',
+        vibrate: [100, 50, 100],
+        data: {
+            url: data.url || '/'
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+// Cosa succede quando il cliente clicca sulla notifica
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
